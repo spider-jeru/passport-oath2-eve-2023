eb0f661a179b42490fc2a89eb517470ddf9b5f0e
Guisch <Guisch@users.noreply.github.com>
2019-03-06 12:04:11 +0100

    remove Google+ scopes

--- example/app.js
+++ example/app.js
@@ -100,11 +100,10 @@ app.get('/login', function(req, res){
 //   redirecting the user to google.com.  After authorization, Google
 //   will redirect the user back to this application at /auth/google/callback
 app.get('/auth/google', passport.authenticate('google', { scope: [
-       'https://www.googleapis.com/auth/plus.login',
-       'https://www.googleapis.com/auth/plus.profile.emails.read'] 
+       'email', 'profile'] 
 }));
 
-// GET /auth/google/callback
+// GET /auth/goofgle/callback
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
